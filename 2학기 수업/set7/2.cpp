fb6fe5885ddcd14f915b224114b4cf7bd83b5547
allium0917 <sistus0917@gmail.com>
2025-11-20 10:52:19 +0900

    과제다했당ㅎㅎㅎㅎㅎㅎㅎ

--- 2학기 수업/set7/2.cpp
+++ 2학기 수업/set7/2.cpp
@@ -8,15 +8,6 @@ typedef struct Node {
     struct Node* right;
 } Node;
 
-// Node* search(Node* root, int key) {
-//     while (root != NULL) {
-//         if (root->data == key) return root;
-//         else if (root->data < key) root = root->right;
-//         else root = root->left;
-//     }
-//     return NULL;
-// }
-
 Node* search(Node* root, int key) {
     if (root == NULL) return NULL;
 
@@ -24,7 +15,6 @@ Node* search(Node* root, int key) {
     else if (root->data < key) return search(root->right, key);
     else return search(root->left, key);
 }
-
 Node* insert(Node* root, int key) {
     if (root == NULL) {
         Node* newNode = (Node*)malloc(sizeof(Node));
@@ -38,7 +28,6 @@ Node* insert(Node* root, int key) {
 
     return root;
 }
-
 Node* del(Node* root, int key){
     if (root == NULL) return NULL;
 
@@ -69,18 +58,17 @@ Node* del(Node* root, int key){
     return root;
 }
 
-// int H(Node* root) {
-//     if (root == NULL) return -1;
-//     int lH = H(root->left);
-//     int rH = H(root->right);
-//     return (lH > rH ? lH : rH) + 1;
-// } 실수로 간선 기준으로 생각해버려써요....
+//경우1:왼쪽만(둘다 < 현노드) -> 재귀
+//경우2:오른쪽만(둘다 > 현노드) -> 재귀
+//경우3:왼쪽 하나 오른쪽 하나
 
-int H(Node* root) {
-    if (root == NULL) return 0;
-    int lH = H(root->left);
-    int rH = H(root->right);
-    return (lH > rH ? lH : rH) + 1;
+Node* find(Node* root, int n1, int n2) {
+    if (root == NULL) return NULL;
+    if (root->data > n1 && root->data > n2)
+        return find(root->left, n1, n2);
+    if (root->data < n1 && root->data < n2)
+        return find(root->right, n1, n2);
+    return root;
 }
 
 void printTree(Node* root, int level) {
@@ -110,9 +98,9 @@ int main() {
         printf("없\n");
 
     printTree(root, 0);
-    root = del(root, 20);
-    printTree(root, 0);
-    printf("%d\n", H(root));
+
+    Node* lca = find(root, 10, 30);
+    printf("%d\n", lca->data);
 
     return 0;
 }
